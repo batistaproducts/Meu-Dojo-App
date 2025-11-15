b069043bdd61fc09d51dba20e13fcd4bfec504c5
batistaproducts <antoniobatistaproducts@gmail.com>
2025-11-15 17:13:33 -0300

    feat: Refactor theme logic and logo component
    Updates the theme application logic to correctly handle dark mode on desktop and enforce light mode on mobile.
    Refactors the Logo component to replace the "JOJO" group with a "DOJO" group, adjusting the SVG path data to represent the letter 'D' and modifying the positioning.

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) 
       {...props}
     >
       <g fill="currentColor">
-        {/* MEU group - positioned over the first J */}
+        {/* MEU group - mantido, mas não essencial para DOJO */}
         <g id="meu" transform="translate(7.5, 0)">
           {/* M */}
           <rect x="0" y="0" width="3" height="15" />
@@ -29,17 +29,18 @@ const Logo: React.FC<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) 
           <rect x="33" y="12" width="12" height="3" />
         </g>
         
-        <g id="jojo" transform="translate(0, 20)">
-          {/* J at x=0 */}
-          <g id="J1">
-            <rect x="45" y="0" width="15" height="20" />
-            <rect x="0" y="45" width="60" height="60" />
-            <rect x="0" y="45" width="60" height="30" />
-            <rect x="0" y="45" width="60" height="30" />
-            <rect x="0" y="15" width="15" height="30" />
+        <g id="dojo" transform="translate(0, 20)">
+          {/* D at x=0 (NOVA LETRA D) */}
+          <g id="D1">
+            {/* Haste vertical do D */}
+            <rect x="0" y="0" width="15" height="60" />
+            {/* Semicírculo/Barriga do D - Usando 3 retângulos para imitar o formato do O */}
+            <rect x="0" y="0" width="60" height="15" />   {/* Traço superior */}
+            <rect x="0" y="45" width="60" height="15" />  {/* Traço inferior */}
+            <rect x="45" y="0" width="15" height="60" /> {/* Traço da direita (fechando) */}
           </g>
           
-          {/* O at x=75 */}
+          {/* O at x=75 (Mantido) */}
           <g id="O1" transform="translate(75, 0)">
             <rect x="0" y="0" width="15" height="60" />
             <rect x="45" y="0" width="15" height="60" />
@@ -47,14 +48,14 @@ const Logo: React.FC<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) 
             <rect x="0" y="45" width="60" height="15" />
           </g>
           
-          {/* J at x=150 */}
+          {/* J at x=150 (Mantido) */}
           <g id="J2" transform="translate(150, 0)">
             <rect x="45" y="0" width="15" height="60" />
             <rect x="0" y="45" width="60" height="15" />
             <rect x="0" y="15" width="15" height="30" />
           </g>
           
-          {/* O at x=225 */}
+          {/* O at x=225 (Mantido) */}
           <g id="O2" transform="translate(225, 0)">
             <rect x="0" y="0" width="15" height="60" />
             <rect x="45" y="0" width="15" height="60" />
